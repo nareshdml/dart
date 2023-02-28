8c01147f57d6dcdc807851d79a544aea17516beb
Naresh Dhimal <dreamnarus99@gmail.com>
2023-02-28 12:37:41 +0545

    variable name and its access...

--- main.dart
+++ main.dart
@@ -1,1 +1,18 @@
 //Dart language is started....
+void main() {
+  //how dart language is function
+  print("Naresh");
+
+  //variables name and access
+  var name = "Naresh";
+  print(name);
+
+  var f_name = "Naresh Dhimal";
+  print(f_name);
+
+  var address1 = "Urlabari";
+  print(address1);
+
+  var $money = 1200;
+  print($money);
+}
