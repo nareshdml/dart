f27b2b5f856aecd2375fb1f260dd3b6b3429ec5a
Naresh Dhimal <dreamnarus99@gmail.com>
2023-02-28 13:28:41 +0545

    Operators

--- main.dart
+++ main.dart
@@ -39,4 +39,72 @@ void main() {
   print(a / b); //0.8333334
   print(a ~/ b); //0 "gives perfect number"
   print(a % b); //5 "gives remanider"
+
+  //##Increment and Decrement
+  var m = 10;
+  //prefix increment
+  print(m); //10
+  print(++m); //11
+  print(m); //11
+
+  var n = 10;
+  //post increment
+  print(n); //10
+  print(n++); //10
+  print(n);
+
+  var x = 10;
+  //prefix decrement
+  print(x); //10
+  print(--x); //9
+  print(x); //9
+
+  var y = 10;
+  //post decrement
+  print(y); //10
+  print(y--); //10
+  print(y); //9
+
+  //##Eqaulity and Relational
+  var e = 13;
+  var f = 14;
+
+  print(e == f); //false
+  print(e != f); //true
+  print(e > f); //false
+  print(e < f); //true
+  print(e >= f); //fasle
+  print(e <= f); //true
+
+  //##Logical
+  //And operators
+  print(true && true); //true
+  print(true && false); //false
+  print(false && true); //false
+  print(false && false); //false
+
+  //Or operator
+  print(true || true); //true
+  print(true || false); //true
+  print(false || true); //true
+  print(false || false); //false
+
+  //##Type testing
+  var q = "Naresh";
+  print(q is String); //true
+  print(q is! String); //false
+
+  var w = 12;
+  print(w is int); //true
+  print(w is! int); //false
+
+  //##Assignmet
+  var t = 5;
+  print(t); //5
+  t += 5;
+  print(t); //10
+  t -= 5;
+  print(t); //5
+  t *= 5;
+  print(t); //25
 }
