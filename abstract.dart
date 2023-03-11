fe65acafd652d1b758b355d56d02d6120bf7761f
Naresh Dhimal <dreamnarus99@gmail.com>
2023-03-11 11:00:39 +0545

    Abstract class and abstract method...

--- abstract.dart
+++ abstract.dart
@@ -1,1 +1,32 @@
 //Abstract class and Abstract method
+void main() {
+  var data = Son();
+  data.showDisplay();
+  data.showdata();
+}
+
+abstract class Father {
+  var name = "Naresh";
+  display() {
+    print("My name is naresh dhimal."); //methods
+  }
+
+  showDisplay(); //abstract method
+  showdata(); //abstract method
+}
+
+class Son extends Father {
+  @override
+  //Defining asbtract method
+  showDisplay() {
+    print(name);
+  }
+
+  //defining abstract method
+  showdata() {
+    var a = 5;
+    int b = 7;
+    print("The sum of $a and $b is ${a + b}");
+    super.display();
+  }
+}
