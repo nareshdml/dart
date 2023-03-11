f3ba89a9764c529360c0de7a1d27164a4a36f116
Naresh Dhimal <dreamnarus99@gmail.com>
2023-03-11 12:15:53 +0545

    import package...

---
+++ colors.dart
@@ -0,0 +1,15 @@
+import 'import/black.dart';
+
+import 'import/purple.dart';
+import 'dart:math';
+
+void main() {
+  blackColor();
+  purplecolor();
+  var black_obj = Black();
+  black_obj.display();
+
+  var purple_obj = Purple();
+  purple_obj.display();
+  print(sqrt(225));
+}
